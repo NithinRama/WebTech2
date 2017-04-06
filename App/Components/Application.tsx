e11ad19c27bfbad0356f83335bf7b3da17b44f56
Abhishek Vijay <Abhishek Vijay>
2017-04-06 15:16:50 +0530

    Added App bar

--- App/Components/Application.tsx
+++ App/Components/Application.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import * as injectTapEventPlugin from 'react-tap-event-plugin';
+import HomePage from './HomePage.tsx';
 
 class Application extends React.Component<undefined, undefined>
 {
--- App/Components/HomePage.tsx
+++ App/Components/HomePage.tsx
@@ -1,1 +1,14 @@
-    import * as React from 'react';
+import * as React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+class HomePage extends React.Component<undefined, undefined>
+{   
+    render()
+    {
+        return (
+            <AppBar title = "CodeGram"/>
+        );
+    }
+}
+
+export default HomePage;
