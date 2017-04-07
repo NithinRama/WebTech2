b78fe842cf901075c2f42fe0b2186b84a57dbca6
Abhishek LAB <Abhishek LAB>
2017-04-07 12:35:04 +0530

    Added CodePage

---
+++ App/Components/CodePage.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+class CodePage extends React.Component<undefined, undefined>
+{
+    render()
+    {
+        return <div>code page</div>
+    }
+}
--- App/Components/MainApplication.tsx
+++ App/Components/MainApplication.tsx
@@ -4,19 +4,42 @@ import RaisedButton from 'material-ui/RaisedButton'
 import FontIcon from 'material-ui/FontIcon'
 import HomePage from './HomePage'
 
-class MainApplication extends React.Component<undefined, undefined>
+class MainApplication extends React.Component<undefined, MainApplicationState>
 {   
+    constructor()
+    {
+        super();
+        this.state = {isLoggedIn: false};
+        this.signIn = this.signIn.bind(this);
+        this.signOut = this.signOut.bind(this);
+    }
+
     render()
     {
-        const appButton = <RaisedButton label = "Sign in" secondary = {true} icon = {<FontIcon className = "fa fa-sign-in"/>} />;
+        let appButton = !this.state.isLoggedIn ? <RaisedButton label = "Sign in" secondary = {true} icon = {<FontIcon className = "fa fa-sign-in"/>} onTouchTap = {this.signIn}/> : <RaisedButton label = "Sign out" secondary = {true} icon = {<FontIcon className = "fa fa-sign-out"/>} onTouchTap = {this.signOut}/> 
 
         return (
             <div>
-                <AppBar title = "CodeGram" iconElementRight = {appButton}/>
-                <HomePage/>
+                <AppBar title = "CodeGram" iconElementRight = {appButton} zDepth = {this.state.isLoggedIn ? 0 : 2}/>
+                {this.state.isLoggedIn ? <CodePage/> : <HomePage/>}
             </div>
         );
+    }
+
+    signIn()
+    {
+        this.setState({isLoggedIn: true});
+    }
+
+    signOut()
+    {
+        this.setState({isLoggedIn: false});
     }
 }
 
 export default MainApplication;
+
+interface MainApplicationState
+{
+    isLoggedIn: boolean;
+}
